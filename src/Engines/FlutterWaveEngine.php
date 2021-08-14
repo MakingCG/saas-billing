222a7e72ba1958877ed6ce410d91326e29efa9dd
MakingCG <MakingCG@users.noreply.github.com>
2021-08-14 10:13:00 +0000

    Fix styling

--- src/Engines/FlutterWaveEngine.php
+++ src/Engines/FlutterWaveEngine.php
@@ -1,12 +1,9 @@
 <?php
 
-
 namespace Makingcg\Subscription\Engines;
-
 
 class FlutterWaveEngine extends Engine
 {
-
     public function hello(): string
     {
         return "Hello, I'm FlutterWave!";
--- src/Engines/StripeEngine.php
+++ src/Engines/StripeEngine.php
@@ -1,12 +1,9 @@
 <?php
 
-
 namespace Makingcg\Subscription\Engines;
-
 
 class StripeEngine extends Engine
 {
-
     public function hello(): string
     {
         return "Hello, I'm Stripe!";
