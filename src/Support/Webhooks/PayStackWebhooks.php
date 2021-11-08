0686ac5d03ec69dad2b44f2da4a529fdf9eafd09
Čarodej <peterpapp@makingcg.com>
2021-11-08 10:35:44 +0100

    customer not found

--- src/Support/Webhooks/PayStackWebhooks.php
+++ src/Support/Webhooks/PayStackWebhooks.php
@@ -1,7 +1,9 @@
 <?php
 namespace VueFileManager\Subscription\Support\Webhooks;
 
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 use VueFileManager\Subscription\Domain\Customers\Models\Customer;
 use VueFileManager\Subscription\Domain\Subscriptions\Models\Subscription;
 
@@ -9,10 +11,18 @@ class PayStackWebhooks
 {
     public function handleSubscriptionCreate(Request $request): void
     {
+        $customerCode = $request->input('data.customer.customer_code');
+
         // Get existing customer
         $customer = Customer::where('driver', 'paystack')
-            ->where('driver_user_id', $request->input('data.customer.customer_code'))
-            ->firstOrFail();
+            ->where('driver_user_id', $customerCode)
+            ->first();
+
+        if (! $customer) {
+            Log::error("Customer with id $customerCode do not exist. We can't perform subscription.create");
+
+            throw new ModelNotFoundException;
+        }
 
         Subscription::create([
             'user_id'         => $customer->user_id,
